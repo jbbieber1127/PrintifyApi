fcd6100c0bd58123b4f46fa32930434617331ea5
John Bieber <jbieber1127@gmail.com>
2023-10-03 11:31:37 -0400

    Uploads (#2)
    * clean comment
    
    * done w uploads

---
+++ V1/Models/Uploads/ImageUploadRequest.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace PrintifyApi.V1.Models.Uploads
+{
+    public abstract class ImageUploadRequest
+    {
+        [JsonProperty("file_name")]
+        public string FileName { get; set; }
+    }
+}
