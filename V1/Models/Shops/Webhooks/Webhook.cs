9e80e6ed68254dbec2a38c1e1ca853d311942239
John Bieber <jbieber1127@gmail.com>
2023-10-03 16:09:48 -0400

    done with webhooks (#3)

--- V1/Models/Shops/Webhooks/Webhook.cs
+++ V1/Models/Shops/Webhooks/Webhook.cs
@@ -1,10 +1,19 @@
-﻿namespace PrintifyApi.V1.Models.Shops.Webhooks
+﻿using Newtonsoft.Json;
+
+namespace PrintifyApi.V1.Models.Shops.Webhooks
 {
     public class Webhook
     {
+        [JsonProperty("id")]
         string Id { get; set; }
+
+        [JsonProperty("shop_id")]
         int ShopId { get; set; }
+
+        [JsonProperty("topic")]
         string Topic { get; set; }
+
+        [JsonProperty("url")]
         string Url { get; set; }
     }
 }
---
+++ V1/Models/Shops/Webhooks/WebhookUpdateRequest.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace PrintifyApi.V1.Models.Shops.Webhooks
+{
+    public class WebhookUpdateRequest
+    {
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+}
