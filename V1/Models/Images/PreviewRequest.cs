93cec01b4ea4912126e0534593d6f87ab073b41e
jbbieber1127 <jbieber1127@gmail.com>
2023-11-06 13:06:31 -0500

    update naming style

--- V1/Models/Images/PreviewRequest.cs
+++ V1/Models/Images/PreviewRequest.cs
@@ -1,58 +1,102 @@
-﻿namespace PrintifyApi.V1.Models.Images
+﻿using Newtonsoft.Json;
+
+namespace PrintifyApi.V1.Models.Images
 {
     // may want to inherit from image class in uploads
     public class Image
     {
-        public double scale { get; set; }
-        public double x { get; set; }
-        public double y { get; set; }
-        public int angle { get; set; }
-        public bool flipX { get; set; }
-        public bool flipY { get; set; }
-        public string layerType { get; set; }
-        public string type { get; set; }
-        public string color { get; set; }
-        public string name { get; set; }
-        public string textAlign { get; set; }
-        public string id { get; set; }
-        public string fontFamily { get; set; }
-        public int fontWeight { get; set; }
-        public int lineHeight { get; set; }
-        public int baselineFontSize { get; set; }
-        public string textInput { get; set; }
-        public int width { get; set; }
-        public int height { get; set; }
+        [JsonProperty("scale")]
+        public double Scale { get; set; }
+
+        [JsonProperty("x")]
+        public double X { get; set; }
+
+        [JsonProperty("y")]
+        public double Y { get; set; }
+
+        [JsonProperty("angle")]
+        public int Angle { get; set; }
+        [JsonProperty("flipX")]
+        public bool FlipX { get; set; }
+
+        [JsonProperty("flipY")]
+        public bool FlipY { get; set; }
+        [JsonProperty("layerType")]
+        public string LayerType { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("color")]
+        public string Color { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("textAlign")]
+        public string TextAlign { get; set; }
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("fontFamily")]
+        public string FontFamily { get; set; }
+        [JsonProperty("fontWeight")]
+        public int FontWeight { get; set; }
+        [JsonProperty("lineHeight")]
+        public int LineHeight { get; set; }
+        [JsonProperty("baselineFontSize")]
+        public int BaselineFontSize { get; set; }
+        [JsonProperty("textInput")]
+        public string TextInput { get; set; }
+        [JsonProperty("width")]
+        public int Width { get; set; }
+        [JsonProperty("height")]
+        public int Height { get; set; }
     }
 
     public class Placeholder
     {
-        public List<string> dom_id { get; set; }
-        public string position { get; set; }
-        public int sequence { get; set; }
-        public bool printable { get; set; }
-        public List<Image> images { get; set; }
+        [JsonProperty("dom_id")]
+        public List<string> DomId { get; set; }
+        [JsonProperty("position")]
+        public string Position { get; set; }
+        [JsonProperty("sequence")]
+        public int Sequence { get; set; }
+        [JsonProperty("printable")]
+        public bool Printable { get; set; }
+        [JsonProperty("images")]
+        public List<Image> Images { get; set; }
     }
 
     public class Print
     {
-        public List<Placeholder> placeholders { get; set; }
-        public bool print_on_side { get; set; }
-        public bool mirror { get; set; }
-        public bool canvas { get; set; }
-        public string font_color { get; set; }
-        public string country { get; set; }
+        [JsonProperty("placeholders")]
+        public List<Placeholder> Placeholders { get; set; }
+        [JsonProperty("print_on_side")]
+        public bool PrintOnSide { get; set; }
+        [JsonProperty("mirror")]
+        public bool Mirror { get; set; }
+        [JsonProperty("canvas")]
+        public bool Canvas { get; set; }
+        [JsonProperty("font_color")]
+        public string FontColor { get; set; }
+        [JsonProperty("country")]
+        public string Country { get; set; }
     }
 
     public class PreviewRequest
     {
-        public Print print { get; set; }
-        public int decorator_id { get; set; }
-        public int camera_id { get; set; }
-        public int variant_id { get; set; }
-        public int size { get; set; }
-        public int blueprint_id { get; set; }
-        public string mockup_mode { get; set; }
-        public string format { get; set; }
+        [JsonProperty("print")]
+        public Print Print { get; set; }
+        [JsonProperty("decorator_id")]
+        public int DecoratorId { get; set; }
+        [JsonProperty("camera_id")]
+        public int CameraId { get; set; }
+        [JsonProperty("variant_id")]
+        public int VariantId { get; set; }
+        [JsonProperty("size")]
+        public int Size { get; set; }
+        [JsonProperty("blueprint_id")]
+        public int BlueprintId { get; set; }
+        [JsonProperty("mockup_mode")]
+        public string MockupMode { get; set; }
+        [JsonProperty("format")]
+        public string Format { get; set; }
     }
 
 
